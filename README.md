1b31effe5944c3692e991aafcddf127bb1af9598
Kathryn Choi <71402592+khmchoi@users.noreply.github.com>
2021-03-24 22:20:37 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Links:
+https://www.youtube.com/watch?v=BmP7mhW-HVU&feature=emb_logo
+
+https://medium.com/@atingenkay/getting-started-with-ejs-28fe4a693e53
+
+https://jcharistech.wordpress.com/2018/09/25/embedding-machine-learning-in-express-js/
+
+https://colah.github.io/posts/2015-08-Understanding-LSTMs/
+
+
+# Tech stack:
+- BE: Node.js, Express Server
+- FE: EJS, Materialize CSS
+- ML: Brain.js
+
+# Get Started:
+- Run nodemon app.js
---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const activities = require('./recs')
+
+// ML:
+const brain = require ('brain.js')
+const data = require('./sentimentData.json');
+const recs = require('./recs');
+
+const network = new brain.recurrent.LSTM();
+
+const trainingData = data.map(item => ({
+  input: item.input,
+  output: item.output
+}));
+
+network.train(trainingData, {
+  iterations: 30,
+  // errorThresh: 0.019,
+  log: (status) => console.log(status)
+});
+
+
+// EXPRESS:
+const app = express();
+
+app.set('view engine', 'ejs')
+
+const urlencodedParser = bodyParser.urlencoded({extended:false})
+
+app.use('/public', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.get('/getStarted', function (req, res) {
+  res.render('name')
+})
+
+app.post('/getStarted', urlencodedParser, function (req, res) {
+  const name = req.body.name
+  console.log(`Hello ${name}`)
+  res.render('intake', {
+    name: req.body.name,
+  })
+})
+
+app.post('/predict', urlencodedParser, function (req, res) {
+  // console.log(req.body.message)
+  console.log(req.body.message)
+  const result = network.run(req.body.message) // runs user input through model;
+  console.log(`sentiment: ${result}`)
+  res.render('results', {
+    myData: req.body.message,
+    resultData: result,
+    activities: activities
+  })
+})
+
+const PORT = 3000
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`)
+})
+
+// const stats = network.train(trainingData);
+// console.log(stats)
+
